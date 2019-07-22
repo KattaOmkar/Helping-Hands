79b7e36335991a24e4d553f2d47d70677dbf5d49
dishesh <dishjani@gmail.com>
2019-07-22 08:33:32 -0700

    myLendings-myBorrows-2
    Signed-off-by: dishesh <dishjani@gmail.com>

---
+++ app/src/main/java/omkar/com/helpinghands/activity/investor.java
@@ -0,0 +1,15 @@
+package omkar.com.helpinghands.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import omkar.com.helpinghands.R;
+
+public class investor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_investor);
+    }
+}
